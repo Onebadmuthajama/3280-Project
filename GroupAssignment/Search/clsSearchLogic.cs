4d85c03c60ee7e2dc260ef37f37a01cdece26275
Jordan Mai <jordanmai@mail.weber.edu>
2019-12-05 18:07:11 -0700

    started search segment

---
+++ GroupAssignment/Search/clsSearchLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment.Search
+{
+    class clsSearchLogic
+    {
+        clsSearchSQL sSQL;
+
+        public clsSearchLogic()
+        {
+            sSQL = new clsSearchSQL();
+        }
+
+        /// <summary>
+        /// Returns all items from Invoices
+        /// </summary>
+        /// <returns></returns>
+        public DataSet getItems()
+        {
+            return sSQL.GetItems();
+        }
+    }
+}
---
+++ GroupAssignment/Search/clsSearchSQL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace GroupAssignment.Search
+{
+    class clsSearchSQL
+    {
+        clsDataAccess db;
+
+        public clsSearchSQL() {
+            db = new clsDataAccess();
+        }
+
+        /// <summary>
+        /// Returns all items from Invoices
+        /// </summary>
+        /// <returns></returns>
+        public DataSet GetItems()
+        {
+            return db.ExecuteSqlStatement("select * from Invoices");
+        }
+    }
+}
